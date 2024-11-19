b52544ca841c72728746cce896046f833f7e0006
Elvira van der Ven <sitde@hr.nl>
2024-11-19 11:36:05 +0100

    first java file (Hello World)

---
+++ LesVoorbeelden/src/Main.java
@@ -0,0 +1,10 @@
+/* *****************************
+ * Author: Elvira van der Ven  *
+ * Date:   19/11/2024          *                                                                                          *
+ *******************************/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!");
+    }
+}
