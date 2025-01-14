2a854822edacba743d75f68bafaf6c3112a6f107
Elvira van der Ven <sitde@hr.nl>
2025-01-14 13:09:06 +0100

    Extra voorbeeld met static en overerving, speciaal voor Dino ;)

---
+++ LesVoorbeelden/src/les14/Auto.java
@@ -0,0 +1,8 @@
+/* *****************************
+ * Author: Elvira van der Ven  *                                                   
+ * Date:   14/01/2025          *                                                                                          *
+ *******************************/
+
+package les14; 
+public class Auto {
+}
